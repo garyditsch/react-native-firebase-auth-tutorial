dbe03dc9abfeed9617f03945efcac583b714c40d
Gary Ditsch <garyditsch@gmail.com>
2017-08-11 13:29:35 -0400

    react native with firebase auth tutorial

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, CardSection, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDk6ml8JDvnu5LuEy62So9MIn2-H57nVak",
+            authDomain: "auth-57120.firebaseapp.com",
+            databaseURL: "https://auth-57120.firebaseio.com",
+            projectId: "auth-57120",
+            storageBucket: "auth-57120.appspot.com",
+            messagingSenderId: "825511459271"
+          });
+
+          firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+          });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return
+                    <CardSection>
+                        <Button onPress={() => firebase.auth.signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>;
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />;
+        }
+    }
+
     render() {
         return (
             <View>
-                <Text>An App!</Text>
+                <Header headerText="Authentication" />
+                {this.renderContent()}
             </View>
         );
     }
